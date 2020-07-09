5cc0f92db3fa2710b953834c08c2de75da54a94e
hyunjigonji <wew9955@naver.com>
2020-07-09 14:35:34 +0900

    split app into models and app

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import os
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-dbfile = os.path.join(basedir, 'db.sqlite')
+from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #트랜잭션이 끝날 때마다 커밋을 함
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
         return render_template('hello.html')
+
+if __name__ == "__main__":
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        dbfile = os.path.join(basedir, 'db.sqlite')
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+        app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #트랜잭션이 끝날 때마다 커밋을 함
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        db.init_app(app)
+        db.app = app
+        db.create_all()
+        
+        app.run(host='127.0.0.1', port=5000, debug=True)
