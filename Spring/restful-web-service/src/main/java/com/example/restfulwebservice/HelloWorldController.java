3e099a2a4e55fdb51acb2aff33b7f17c7619b93f
이현지 <hyunjigonji@ihyeonjiui-MacBookPro.local>
2020-12-22 17:09:59 +0900

    created HelloWorld controller

---
+++ Spring/restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (end-point)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
