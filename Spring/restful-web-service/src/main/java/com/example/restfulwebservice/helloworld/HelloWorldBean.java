bc5d76ef0cee116269cb39d5265e7f07ab4dddfc
hyunjigonji <guswl5404@hanyang.ac.kr>
2020-12-26 00:19:47 +0900

    created user domain class and get api

--- Spring/restful-web-service/src/main/java/com/example/restfulwebservice/helloworld/HelloWorldBean.java
+++ Spring/restful-web-service/src/main/java/com/example/restfulwebservice/helloworld/HelloWorldBean.java
@@ -1,11 +1,13 @@
-package com.example.restfulwebservice;
+package com.example.restfulwebservice.helloworld;
 
 // lombok dependency -> getter , setter , etc.
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class HelloWorldBean {
     private String message;
 //    public String getMessage(){
