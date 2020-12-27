5800ffa433a768f28bc7854bc5e786e0e8ce7874
hyunjigonji <guswl5404@hanyang.ac.kr>
2020-12-27 18:58:26 +0900

    jpa

--- Spring/restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ Spring/restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -9,9 +9,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,7 +24,10 @@ import java.util.Date;
 //@JsonIgnoreProperties(value={"password"})
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message="name은 두 글자 이상 입력해주세요")
@@ -37,4 +45,15 @@ public class User {
 //    @JsonIgnore
     @ApiModelProperty(notes = "사용자 주민등록번호를 입력해주세요")
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    public User(int id, String name, Date joinDate, String password, String ssn) {
+        this.id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.password = password;
+        this.ssn = ssn;
+    }
 }
---
+++ Spring/restful-web-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into USER values(101, sysdate(), 'user1', 'test1111', '1111');
+insert into USER values(102, sysdate(), 'user2', 'test2222', '2222');
+insert into USER values(103, sysdate(), 'user3', 'test3333', '3333');
+
+insert into POST values(101, 'My first post', 101);
+insert into POST values(102, 'My second post', 102);
